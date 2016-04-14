1e625d7cdad255219a6178f6c43dadb16fd4c150
liuxu <liuxu@eqxiu.com>
2016-04-14 09:55:28 +0800

    step-1

---
+++ src/components/Element.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Text from './Text';
+import Image from './Image';
+
+export default React.createClass({
+  getElementByType: function(elementDef) {
+  	switch (elementDef.type + '') {
+  		case '2':
+  			return <Text def={elementDef}/>;
+  		case '4':
+  			return <Image def={elementDef}/>;
+  		default:
+  	}
+  },
+  componentDidMount: function() {
+    var dom = ReactDOM.findDOMNode(this);
+    var styles = this.props.def.css;
+    for(var key in styles) {
+    	dom.style[key] = styles[key];
+    }
+    console.log(dom);
+  },
+  render: function() {
+    return <div className="element">
+    	{this.getElementByType(this.props.def)}
+    </div>;
+  }
+});
